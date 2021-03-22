e025ca8d654e1d59b4021f3609f445a795604a59
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-22 18:07:38 -0300

    alert added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     user: [],
     loading: true,
+    alert: null,
   };
 
   async componentDidMount() {
@@ -28,6 +30,13 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -35,10 +44,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users?.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,12 +10,17 @@ export class Search extends Component {
     searchUsers: propTypes.func.isRequired,
     clearUsers: propTypes.func.isRequired,
     showClear: propTypes.bool.isRequired,
+    setAlert: propTypes.func.isRequired,
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
