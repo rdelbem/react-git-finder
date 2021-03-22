3bb6c217e05df308df673ea7e139a9ef55974b29
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-22 17:52:34 -0300

    clear btn

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: propTypes.func.isRequired,
+    clearUsers: propTypes.func.isRequired,
+    showClear: propTypes.bool.isRequired,
   };
 
   onSubmit = (e) => {
@@ -19,6 +21,7 @@ export class Search extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -35,6 +38,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
