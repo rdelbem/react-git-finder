61966b6ff8229c62f1b6cc19d3977d3f9cfb9b30
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-23 18:43:24 -0300

    finished

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <p className="lead">the page dos not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
